31d4a69bfaf9f4b7a67c25feb9b5cc13a2389104
Ron Savage <ron@savage.net.au>
2008-11-13 12:24:27 +1100

    For testing V 3

---
+++ cgi-bin/use.cgi.uploader.v3.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+#
+# Name:
+# use.cgi.uploader.v3.pl.
+#
+# Note:
+# Need use lib here because CGI scripts don't have access to
+# the PerlSwitches used in httpd.conf.
+
+use lib '/home/ron/perl.modules/CGI-Up/lib';
+use strict;
+use warnings;
+
+use CGI::Up::Test;
+
+# ----------------
+
+CGI::Up::Test -> new() -> use_cgi_uploader_v3();
