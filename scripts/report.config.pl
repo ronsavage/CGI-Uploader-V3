e39f000c2ffe4a0744e5e5704e5c7b9c3f79eafd
Ron Savage <ron@savage.net.au>
2008-11-26 10:39:52 +1100

    Simplify now dsn is an arrayref

--- scripts/report.config.pl
+++ scripts/report.config.pl
@@ -12,9 +12,7 @@ use CGI::Uploader::Config;
 
 my($config) = CGI::Uploader::Config -> new();
 
-print 'dsn:         ', $config -> dsn(), ". \n";
+print 'dsn:         ', join(', ', @{$config -> dsn()}), ". \n";
 print 'form_action: ', $config -> form_action(), ". \n";
-print 'password:    ', $config -> password(), ". \n";
 print 'table_name:  ', $config -> table_name(), ". \n";
 print 'tmpl_path:   ', $config -> tmpl_path(), ". \n";
-print 'username:    ', $config -> username(), ". \n";
